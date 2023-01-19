b1734f29ec650ae4591bf57b7b233d4845216776
David Zhao <davidzhao0524@gmail.com>
2023-01-18 21:19:16 -0500

    summitive assignment added cart and other thing

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+</script>
+
+<template>
+  <div class="app-container">
+    <Suspense>
+      <RouterView />
+    </Suspense>
+  </div>
+</template>
+
+<style scoped>
+</style>
